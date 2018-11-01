50a731212b6311c1739a63a693946c719b9556d9
liu hui <im.lh@hotmail.com>
2018-11-01 22:46:14 +0800

    order coding

--- order/src/main/java/com/lh/order/service/impl/OrderServiceImpl.java
+++ order/src/main/java/com/lh/order/service/impl/OrderServiceImpl.java
@@ -1,8 +1,13 @@
 package com.lh.order.service.impl;
 
+import com.lh.order.client.ProductClient;
+import com.lh.order.dataobject.DecreaseStock;
 import com.lh.order.dataobject.OrderDetail;
 import com.lh.order.dataobject.OrderMaster;
+import com.lh.order.dataobject.ProductInfo;
 import com.lh.order.dto.OrderDTO;
+import com.lh.order.enums.OrderStatusEnum;
+import com.lh.order.enums.PayStatusEnum;
 import com.lh.order.repository.OrderDetailRepository;
 import com.lh.order.repository.OrderMasterRepository;
 import com.lh.order.service.OrderService;
@@ -11,7 +16,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -22,33 +31,51 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     OrderMasterRepository orderMasterRepository;
 
+    @Autowired
+    ProductClient productClient;
+
     @Override
+    @Transactional
     public OrderDTO create(OrderDTO orderDTO) {
         String orderId = KeyUtil.genUniqueKey();
         //查询商品信息(调用商品服务)
+        List<String> productIdList = orderDTO.getOrderDetailList().stream()
+                .map(OrderDetail::getProductId)
+                .collect(Collectors.toList());
+        List<ProductInfo> productInfoList = productClient.listForOrder(productIdList);
+
         //计算总价
+        BigDecimal orderAmout = new BigDecimal(BigInteger.ZERO);
+        for (OrderDetail orderDetail: orderDTO.getOrderDetailList()) {
+            for (ProductInfo productInfo: productInfoList) {
+                if (productInfo.getProductId().equals(orderDetail.getProductId())) {
+                    //单价*数量
+                    orderAmout = productInfo.getProductPrice()
+                            .multiply(new BigDecimal(orderDetail.getProductQuantity()))
+                            .add(orderAmout);
+                    BeanUtils.copyProperties(productInfo, orderDetail);
+                    orderDetail.setOrderId(orderId);
+                    orderDetail.setDetailId(KeyUtil.genUniqueKey());
+                    //订单详情入库
+                    orderDetailRepository.save(orderDetail);
+                }
+            }
+        }
+
         //扣库存(调用商品服务)
+        List<DecreaseStock> decreaseStockInputList = orderDTO.getOrderDetailList().stream()
+                .map(e -> new DecreaseStock(e.getProductId(), e.getProductQuantity()))
+                .collect(Collectors.toList());
+        productClient.decreaseStock(decreaseStockInputList);
+
         //订单入库
         OrderMaster orderMaster = new OrderMaster();
         orderDTO.setOrderId(orderId);
-        orderDTO.setOrderStatus(1);
-        orderDTO.setPayStatus(1);
-        orderDTO.setBuyerAddress("address");
-        orderDTO.setBuyerName("name");
-        orderDTO.setBuyerOpenid("openid");
-        orderDTO.setBuyerPhone("153");
-        orderDTO.setOrderAmount(new BigDecimal(1));
         BeanUtils.copyProperties(orderDTO, orderMaster);
+        orderMaster.setOrderAmount(orderAmout);
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
         orderMasterRepository.save(orderMaster);
-        for (OrderDetail orderDetail : orderDTO.getOrderDetailList()) {
-            orderDetail.setOrderId(orderId);
-            orderDetail.setDetailId(KeyUtil.genUniqueKey());
-            orderDetail.setProductName("product name");
-            orderDetail.setProductPrice(new BigDecimal(1));
-            orderDetail.setProductIcon("");
-            orderDetail.setProductQuantity(1);
-            orderDetailRepository.save(orderDetail);
-        }
         return orderDTO;
     }
 
